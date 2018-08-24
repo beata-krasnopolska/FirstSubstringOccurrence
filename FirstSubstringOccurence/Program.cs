a028570ba0ba0d018f5ebd4a46458b25aeab1158
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-08-24 10:15:31 +0200

    Add project files.

---
+++ FirstSubstringOccurence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstSubstringOccurence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "CodefightsIsAwesome";
+            string x = "IA"; // the output should be strstr(s, x) = -1;
+            string y = "IsA"; // the output should be strstr(s, x) = 10
+
+            var check = new SubstringFinder();
+            var findSubstring = check.findFirstSubstringOccurrence(s, x);
+            var findSecSubstring = check.findFirstSubstringOccurrence(s, y);
+
+            Console.WriteLine(findSubstring);
+            Console.WriteLine(findSecSubstring);
+            Console.Read();  
+        }
+    }
+}
---
+++ FirstSubstringOccurence/SubstringFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FirstSubstringOccurence
+{
+    class SubstringFinder
+    {
+        public int findFirstSubstringOccurrence( string longerText, string term)
+        {
+            int found = -1;
+            int termIndex = 0;
+
+            //quick check to protect the user from themselves
+            if (String.IsNullOrEmpty(longerText) || string.IsNullOrEmpty(term))
+                return -1;
+
+            for (int longerTextIndex = 0; longerTextIndex < longerText.Length; longerTextIndex++)
+            {
+                if (term[termIndex] == longerText[longerTextIndex])
+                {
+                    if (termIndex == 0)
+                        found = longerTextIndex;
+                    termIndex++;
+                    if (termIndex >= term.Length)
+                        return found;
+                }
+                else
+                {
+                    termIndex = 0;
+                    if (found >= 0)
+                        longerTextIndex = found;
+                    found = -1;
+                }
+            }
+            return -1;
+        }
+    }
+}
