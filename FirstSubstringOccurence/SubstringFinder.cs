5274219572f8c8f3416cc93783e7be071201b5e1
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-09-02 21:55:34 +0200

    add additional alternative foreach loop

--- FirstSubstringOccurence/SubstringFinder.cs
+++ FirstSubstringOccurence/SubstringFinder.cs
@@ -6,12 +6,33 @@ namespace FirstSubstringOccurence
     {
         public int findFirstSubstringOccurrence( string longerText, string term)
         {
-            int found = -1;
-            int termIndex = 0;
+            var found = -1;
+            var termIndex = 0;
+            var i = 0;
 
             //quick check to protect the user from themselves
             if (String.IsNullOrEmpty(longerText) || string.IsNullOrEmpty(term))
                 return -1;
+            
+            foreach (var letter in longerText)
+            {
+                if (letter == term[termIndex])
+                {
+                    if (termIndex == 0)
+                        found = i;
+                    termIndex++;
+                    if (termIndex >= term.Length)
+                        return found;
+                }
+                else
+                {
+                    termIndex = 0;
+                    if (found >= 0)
+                        i = found;
+                    found = -1;
+                }
+                i++;
+            }
 
             for (int longerTextIndex = 0; longerTextIndex < longerText.Length; longerTextIndex++)
             {
